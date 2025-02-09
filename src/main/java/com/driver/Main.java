341e24b5b7cc1b188dcc0d551fb0e73039ba4080
RajkumarRavadi <rajk142567@gmail.com>
2025-02-09 19:52:43 +0530

    implementing encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        //creating object of RWOnly
+        RWOnly obj = new RWOnly();
+
+        //hence the java name has private access
+        // we will get errors if we try to access it normally
+        //we have to use the getter and setter for private variables
+        //System.out.println(obj.name);
+
+        obj.setName("Rajkumar");
+
+        System.out.println(obj.getName());
+
+
+    }
 }
